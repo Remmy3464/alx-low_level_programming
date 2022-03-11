c1188d224008d3081c7b55cc06024f455bfa8da0
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-03-11 01:17:45 +0000

    quotes.c

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,18 +1,17 @@
 	.file	"main.c"
-	.intel_syntax noprefix
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	push	rbp
+	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	mov	rbp, rsp
+	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	mov	eax, 0
-	pop	rbp
+	movl	$0, %eax
+	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
