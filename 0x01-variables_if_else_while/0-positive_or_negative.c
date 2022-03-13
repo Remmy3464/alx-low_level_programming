1965d245f35cbd0b7b8bd8a91ef3289cfc164305
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-03-13 23:27:34 +0000

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
- * main - Determine if a random number is positive, negative or zero.
+* main - Determine if a random number is positive, negative or zero.
 (*
- * Return: 0 on success
- */
+* Return: Always 0 (Successful)
+*/
 int main(void)
 {
-README.md int n;
+int n;
 
-README.md srand(time(0));
-README.md n = rand() - RAND_MAX / 2;
-README.md /* your code goes there */
-README.md if (n < 0)
-README.md {
-README.md README.md printf("%d is %s\n", n, "negative");
-README.md }
-README.md else if (n > 0)
-README.md {
-README.md README.md printf("%d is %s\n", n, "positive");
-README.md }
-README.md else
-README.md {
-README.md README.md printf("%d is %s\n", n, "zero");
-README.md }
-README.md return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* my code goes there */
+if (n < 0)
+{
+printf("%d is %s\n", n, "negative");
+}
+else if (n > 0)
+{
+printf("%d is %s\n", n, "positive");
+}
+else
+{
+printf("%d is %s\n", n, "zero");
+}
+return (0);
 }
