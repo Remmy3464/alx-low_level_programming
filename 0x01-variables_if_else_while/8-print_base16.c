e281799f1ef9931b16929dfcc6830b510e3cb7f6
Aderemi A <93253562+Remmy3464@users.noreply.github.com>
2022-03-15 00:07:58 +0100

    print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,4 +6,19 @@
  */
 int main(void)
 {
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
