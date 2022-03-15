3c90df8cc26f7be83c0451a8c6ce679035586c5f
Aderemi A <93253562+Remmy3464@users.noreply.github.com>
2022-03-16 00:16:23 +0100

    Emacs file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_alxafrica.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
