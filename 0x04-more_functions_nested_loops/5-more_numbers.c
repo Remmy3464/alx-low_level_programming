b2aa76933787ab153fa7f041169189505ca45ecb
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-03-19 03:01:20 +0000

    two file up

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,7 @@
+/**
+ * more_numbers - prints the range 0-14 ten times.
+ * Return: nothing.
+ */
+void more_numbers(void)
+{
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,7 @@
+/**
+ * print_line - draws a straight line (n) characters long.
+ * @n: the number of underscores to print.
+ */
+void print_line(int n)
+{
+}
