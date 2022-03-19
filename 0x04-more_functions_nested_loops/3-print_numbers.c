796ea3eb10d0c63b5ce94dfb6f1449fa6945cea0
Aderemi A <93253562+Remmy3464@users.noreply.github.com>
2022-03-19 03:57:21 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,12 @@
  */
 void print_numbers(void)
 {
-int i = 0;
-while (i <= 9)
-{
-_putchar(i + 0);
-++i;
-}
-_putchar(n);
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+		++i;
+	}
+	_putchar('\n');
 }
