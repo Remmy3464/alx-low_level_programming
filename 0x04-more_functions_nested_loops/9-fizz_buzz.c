fadec233f6b1f8d2faf5ec560ffb757c35867ad6
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-03-19 03:22:52 +0000

    fizz buzz"

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers 1 - 100,
+ *   instead of multiples of 3, print Fizz
+ *   instead of multiples of 5, print Buzz
+ *   for multiples of 3 and 5, print FizzBuzz.
+(*
+ * Return: 0 on success.
+ */
+int main(void)
+{
+}
