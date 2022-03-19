f46dad70b34920de65f086e07daafed01dca7644
Aderemi A <93253562+Remmy3464@users.noreply.github.com>
2022-03-19 04:03:51 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,4 +4,20 @@
  */
 void more_numbers(void)
 {
+	int i, j;
+
+	i = j = 0;
+	while (i < 10)
+	{
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			++j;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
 }
