917a4250a453f7852d63a4078cb7b2879079af1d
Aderemi A <93253562+Remmy3464@users.noreply.github.com>
2022-03-19 04:20:13 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,4 +5,17 @@
  */
 void print_square(int n)
 {
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
